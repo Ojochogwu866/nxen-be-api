a9fa86c2fbb2d3b0b4e1f018d3988b72bce88e1b
Ojochogwu Dickson <07afrik@gmail.com>
2024-01-10 16:28:10 +0100

    WIP

--- src/router/events.ts
+++ src/router/events.ts
@@ -7,7 +7,7 @@ export default (router: express.Router) => {
     router.post('/events/new-event', isAuthenticated,  newEvent);
     router.post('/events/:id/register', registerForEvent);
     router.get('/events',  getAllEvents);
-    router.get('/events',  getEvent);
+    router.get('/events/:id',  getEvent);
     router.get('/events/:id/registered-users', isAuthenticated, getAllRegisteredUsers)
     router.delete('/events/:id/delete', isAuthenticated,  deleteEvent);
     router.patch('/events/:id/update', isAuthenticated,  updateEvent);
